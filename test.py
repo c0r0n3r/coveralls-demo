d4e5b9d901e0200004412c61d75100edb9bf9196
micah <micah.koga@getbraintree.com>
2016-08-19 13:45:00 -0700

    Remove n=1 test to see code coverage drop

--- test.py
+++ test.py
@@ -8,9 +8,6 @@ class TestFibonacci(unittest.TestCase):
     def test_fibonacci_with_zero_input(self):
         assert fibonacci(0) == 0
 
-    def test_fibonacci_with_one_input(self):
-        assert fibonacci(1) == 1
-
 
 if __name__ == '__main__':
     unittest.main()
